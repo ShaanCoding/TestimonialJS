b8abc1c4c236375e9059d45bf42414d24c6996f5
Shaan Khan <shaankhan101@gmail.com>
2020-12-15 21:56:47 +1100

    deBubz/more JS refactor
    Refactored code

--- js/app.js
+++ js/app.js
@@ -1,52 +1,29 @@
-const customerImage = document.getElementById('customer-img');
-const customerName = document.getElementById('customer-name');
-const customerText = document.getElementById('customer-text');
-const buttons = document.querySelectorAll('btn');
 let index = 0;
-const customers = [];
 
-class Customer {
-    constructor(image, name, quote)
-    {
-        this.image = image;
-        this.name = name;
-        this.quote = quote;
-    }
+// json - pmuch tthe same as es6 classes
+const customers = [
+    { image: './images/people/customer-0.png', name: 'John Doe', quote: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?' },
+    { image: './images/people/customer-1.png', name: 'Sandy Su', quote: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock' },
+    { image: './images/people/customer-2.png', name: 'Amy Zing', quote: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.' },
+    { image: './images/people/customer-3.png', name: 'Tyrell F', quote: 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.' },
+    { image: './images/people/customer-4.png', name: 'Wanda M', quote: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' },
+];
+
+const refreshCard = () => {
+    document.getElementById('customer-img').src = customers[index].image;
+    document.getElementById('customer-name').textContent = customers[index].name;
+    document.getElementById('customer-text').textContent = customers[index].quote;
 }
 
-function createCustomer(image, name, quote)
-{
-    let fullImg = `./images/people/${image}`;
-    let customer = new Customer(fullImg, name, quote);
-    customers.push(customer);
-}
-
-createCustomer("customer-0.png", 'John Doe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
-createCustomer("customer-1.png", 'Sandy Su', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
-createCustomer("customer-2.png", 'Amy Zing', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
-createCustomer("customer-3.png", 'Tyrell F', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
-createCustomer("customer-4.png", 'Wanda M', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
-
-customerImage.src = customers[index].image;
-customerName.textContent = customers[index].name;
-customerText.textContent = customers[index].quote;
-
-let prevBtn = document.getElementById('prevBtn');
-prevBtn.addEventListener("click", function(e)
-{
-    index = (index === 0) ? customers.length : index;
-    index--;
-    customerImage.src = customers[index].image;
-    customerName.textContent = customers[index].name;
-    customerText.textContent = customers[index].quote;
+document.getElementById('prevBtn').addEventListener('click', (e) => {
+    index = ((index === 0) ? customers.length : index) - 1;
+    refreshCard();
 });
 
-let nextBtn = document.getElementById('nextBtn');
-nextBtn.addEventListener("click", function(e)
-{
-    index++;
-    index = (index === customers.length) ? 0 : index;
-    customerImage.src = customers[index].image;
-    customerName.textContent = customers[index].name;
-    customerText.textContent = customers[index].quote;
+document.getElementById('nextBtn').addEventListener('click', (e) => {
+    index = ++index % customers.length;
+    refreshCard();
 });
+
+// -----------------------------------------------------------
+refreshCard();
